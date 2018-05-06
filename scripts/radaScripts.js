eb5cf11a5190c431ebe6b4315e6e995ce6605b6b
Pawel Ciupka <pawelciupka@gmail.com>
2018-05-06 12:12:48 +0200

    After fourth correction.

--- scripts/radaScripts.js
+++ scripts/radaScripts.js
@@ -2,4 +2,5 @@ document.getElementById('scrollToStart').onclick = function() { scrollUpToElemen
 document.getElementById('toPoznajNas').onclick = function() { scrollDownToElement('PoznajNas'); };    
 document.getElementById('toNaszaDzialalnosc').onclick = function() { scrollDownToElement('NaszaDzialalnosc'); };
 document.getElementById('toDolaczDoNas').onclick = function() { scrollDownToElement('DolaczDoNas'); };
+document.getElementById('toRekrutacja').onclick = function() { scrollDownToElement('Rekrutacja'); };
 document.getElementById('toAkcjaAnkietyzacja').onclick = function() { scrollDownToElement('AkcjaAnkietyzacja'); };
